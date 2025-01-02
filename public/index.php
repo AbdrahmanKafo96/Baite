d19bde78f2f0322456f8c04997198a480440421f
Abdulrahman Kafu <73119428+AbdrahmanKafo96@users.noreply.github.com>
2025-01-02 18:23:10 +0200

    fix bug

--- public/index.php
+++ public/index.php
@@ -1,105 +1,55 @@
-<!DOCTYPE html>
-<html lang="ar" dir="rtl">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Store</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css
-    ">
-    <link rel="stylesheet" href="auth/css/main.css">
-</head>
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-<body>
+define('LARAVEL_START', microtime(true));
 
-    <!-- ####### -->
-    <!-- Start of the navbar -->
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+|
+| If the application is in maintenance / demo mode via the "down" command
+| we will load this file so that any pre-rendered content can be shown
+| instead of starting the framework, which could cause an exception.
+|
+*/
 
-    <nav class="navbar navbar-expand-lg">
-        <div class="container">
-            <a class="navbar-brand" href="index.php">الشعار</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.php">الرئيسية</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">نبذة عنا</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">خدمات الصيانة المنزلية</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">خدمات قطاع الأعمال</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">التسجيل كأجير</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"> المدونة الرقمية</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#"> التوظيف</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">تواصل معنا</a>
-                    </li>
-                    <a href="auth/login.php" type="button" class="btn btn-primary operationButton">تسجيل الدخول</a>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <!-- End of the navbar -->
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
-    <!-- Start of the Hero Background -->
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| this application. We just need to utilize it! We'll simply require it
+| into the script here so we don't need to manually load our classes.
+|
+*/
 
-    <section id="hero-background">
-        <!-- Hero background to be added Later ! -->
-    </section>
+require __DIR__.'/../vendor/autoload.php';
 
-    <!-- End of the Hero Background -->
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request using
+| the application's HTTP kernel. Then, we will send the response back
+| to this client's browser, allowing them to enjoy our application.
+|
+*/
 
-    <!-- Start of the main article  -->
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-    <article id="main">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-6">
-                    <h1>عن تطبيقنا للصيانة</h1>
-                    <p>تطبيق للصيانة هو منصة إلكترونية لتوفير خدمات صيانة، مع ربطك بأفضل مزودي الخدمات خلال دقيقتين فقط مع توفير افضل الأسعار وضمان للعمل
-                    </p>
-                </div>
-                <div class="col-12 col-md-6">
-                    <img class="img-fluid" src="auth/images/services.png" alt="services">
-                </div>
-            </div>
-        </div>
-    </article>
+$kernel = $app->make(Kernel::class);
 
-    <!-- End of the main article  -->
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
 
-    <!-- Start of the second article  -->
-
-    <article id="service-advantages" class="mt-5 p-4">
-        <div class="container text-center">
-            <div class="wrapper py-5">
-                <h2>لماذا تختار تطبيقنا؟</h2>
-                <span>يعتبر تطبيق أجير للصيانة من أفضل التطبيقات السعودية في الصيانة من خلال الجوال
-                </span>
-            </div>
-            <div class="d-flex justify-content-center p-3 text-white">
-                <div class="p-2 bg-info me-3">Flex item 1</div>
-                <div class="p-2 bg-warning me-3">Flex item 2</div>
-                <div class="p-2 bg-primary me-3">Flex item 3</div>
-            </div>
-        </div>
-    </article>
-
-    <!-- End of the second article  -->
-
-    <!-- the reference to main.js shall be removed later -->
-    <?php include 'auth/include/template/footer.php' ?>
-    <?php include 'auth/include/template/scripts.php' ?>
+$kernel->terminate($request, $response);
