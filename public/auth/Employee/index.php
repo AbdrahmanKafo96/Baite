32fbe1ba7d14d003f2e64af0c52db8d669f1809d
Zakaria <43708727+zakaria46@users.noreply.github.com>
2025-01-03 16:25:33 +0200

    Functions has been added

---
+++ public/auth/Employee/index.php
@@ -0,0 +1,7 @@
+<?php include './include/template/header.php' ?>
+<h1>أهلا بك إلى لوحة تحكم الموظف </h1>
+<a href="../../index.php" type="button" class="btn btn-primary mt-4">العودة الى الصفحة الرئيسية</a>
+<a type="button" class="btn btn-danger mt-4" onclick="logOut()">تسجيل الخروج</a>
+
+<script src="./js/adminScript.js"></script>
+<!-- <?php include '../include/template/scripts.php' ?> -->
---
+++ public/auth/employeeLogin/include/template/footer.php
@@ -0,0 +1,20 @@
+<footer>
+    <div class="container">
+        <div class="row mx-auto">
+            <div class="col-md-6 pe-5 my-5">
+                <h5 class="mb-3">عن تطبيقنا</h5>
+                <a href="#"> <span class="d-block mb-2">نبذة عنا</span>
+                </a>
+                <a href="#"><span class="d-block mb-2">سياسة الخصوصية</span></a>
+                <a href="#"> <span class="d-block mb-2">الأسئلة الشائعة</span>
+                </a>
+            </div>
+            <div class="col-md-6 my-5">
+                <h5 class="mb-3">تواصل معنا</h5>
+                <span class="d-block mb-3">test@test.com</span>
+                <span class="d-block mb-3">شارع شارع رقم شارع</span>
+                <span class="d-block mb-3">0000001</span>
+            </div>
+        </div>
+    </div>
+</footer>
